90d9718e64f3b1ba8c0fb0b9454ea5c66b7d5a40
devmuddasir <161027324+devmuddasir@users.noreply.github.com>
2024-10-26 21:43:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// GSAP Animations for Elements
-document.querySelectorAll("[data-gsap]").forEach(el => {
-    gsap.fromTo(el, { opacity: 0, y: 20 }, {
-        opacity: 1, y: 0, duration: 1.2,
-        scrollTrigger: { trigger: el, start: "top 90%", end: "bottom 60%", scrub: true }
-    });
-});
-
 
